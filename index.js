4c5f8c249272c9b19e1367351ad4c4bb98bfc9fc
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-03-03 19:27:04 +0000

    added engineers prompt questions and tested in node to ensure all working

--- index.js
+++ index.js
@@ -18,12 +18,73 @@ const idList = []
 const teamMembers = []
 
 const appMenu = ()=>{
+    function buildTeam(){
+
+    }
+    function addIntern(){
+        
+    }
+    function addEngineer(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "what is your engineer's name?",
+                validate: answer =>{
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "please input engineer's name."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "what is your engineer's ID?",
+                validate: answer =>{
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "please input engineer's ID."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "what is your engineer's email address?",
+                validate: answer =>{
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "please input engineer's email address."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "what is your engineer's Githib Username?",
+                validate: answer =>{
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "please input engineer's Githib Username."
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub,);
+            teamMembers.push(engineer);
+            idList.push(answers.engineerId);
+            console.log(engineer)
+            createTeam()
+        })
+    }
+
     function createTeam(){
         inquirer.prompt([
             {
                 type: "list",
                 name: "memberChoice",
-                message: "Which Team member would you like to choode?",
+                message: "Which Team member would you like to choose?",
                 choices: [
                     "Engineer",
                     "Intern",
@@ -32,12 +93,15 @@ const appMenu = ()=>{
             }
         ]).then(userChoice =>{
             if(userChoice.memberChoice === "Engineer"){
-
+                /* BUILD ENGINEER TO ADD HERE */
+                addEngineer();
             }else if(userChoice.memberChoice === "Inter"){
-
+                /* BUILD INTERN TO ADD HERE */
+                addIntern();
             }
             else{
-                
+                /* BUILD WITHOUT CHOICE TO ADD HERE */
+                buildTeam();
             }
         })
     }
@@ -91,6 +155,7 @@ const appMenu = ()=>{
             },
         ]).then(answers =>{
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            console.log(manager);
             teamMembers.push(manager);
             idList.push(answers.managerId);
             createTeam();
